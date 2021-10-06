dbfe24279cab44b9fd522c9f712e3318ae7f7bf2
RadeDev <bajic.rade2@gmail.com>
2021-10-06 23:39:33 +0200

    🦉

--- .babelrc.js
+++ .babelrc.js
@@ -8,6 +8,18 @@ module.exports = {
           importSource: "@emotion/react",
         },
       },
+      // ---- I ADDED THESE TWO (THIS IS BAD (REMOVE THIS))----
+      /* [
+        "@babel/preset-env",
+        {
+          targets: {
+            node: "current",
+          },
+        },
+      ],
+      "@babel/preset-typescript" */
+      // ---------------------------
+      ,
     ],
   ],
   plugins: ["@emotion/babel-plugin", "babel-plugin-macros", "superjson-next"],
